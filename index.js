535e35342434181e80da0a6e91c3f5904872339f
deathcap <deathcap@gmx.co.uk>
2014-03-23 11:48:45 -0700

    Add display names

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ function DecorativePlugin(game, opts) {
 
 DecorativePlugin.prototype.enable = function() {
   // "storage" blocks
-  this.registry.registerBlock('blockCoal', {texture: 'coal_block'});
-  this.registry.registerBlock('blockIron', {texture: 'iron_block'});
-  this.registry.registerBlock('blockGold', {texture: 'gold_block'});
-  this.registry.registerBlock('blockDiamond', {texture: 'diamond_block'});
+  this.registry.registerBlock('blockCoal', {texture: 'coal_block', displayName: 'Block of Coal'});
+  this.registry.registerBlock('blockIron', {texture: 'iron_block', displayName: 'Block of Iron'});
+  this.registry.registerBlock('blockGold', {texture: 'gold_block', displayName: 'Block of Gold'});
+  this.registry.registerBlock('blockDiamond', {texture: 'diamond_block', displayName: 'Block of Diamond'});
 
   // stone bricks
-  this.registry.registerBlock('stoneBrick', {texture: 'stonebrick'});
-  this.registry.registerBlock('stoneBrickCarved', {texture: 'stonebrick_carved'});
-  this.registry.registerBlock('stoneBrickCracked', {texture: 'stonebrick_cracked'});
-  this.registry.registerBlock('stoneBrickMossy', {texture: 'stonebrick_mossy'});
+  this.registry.registerBlock('stoneBrick', {texture: 'stonebrick', displayName: 'Stone Bricks'});
+  this.registry.registerBlock('stoneBrickCarved', {texture: 'stonebrick_carved', displayName: 'Carved Stone Bricks'});
+  this.registry.registerBlock('stoneBrickCracked', {texture: 'stonebrick_cracked', displayName: 'Cracked Stone Bricks'});
+  this.registry.registerBlock('stoneBrickMossy', {texture: 'stonebrick_mossy', displayName: 'Mossy Stone Bricks'});
 
   // TODO: recipes
 };
